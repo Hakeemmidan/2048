194cb2f1b6eef803825744d2e523c7e60817a231
Hakeem Almidan <hakeemmidan@gmail.com>
2019-10-02 20:34:06 -0700

    Make new cells appear every time you click on an arrow

--- public/javascripts/game.js
+++ public/javascripts/game.js
@@ -7,14 +7,21 @@ export class Game {
         this.cellHeight = cellHeight
         this.gameHeight = gameHeight
         this.padding = padding
+        
+        this.gameStaticObjects = []
+        this.gameMovingObjects = []
     }
+
 
     start() {
         this.background = new Background(this)
-        this.cell = new Cell(this)
-        new InputHandler(this.cell)
-        this.gameStaticObjects = [ this.background ]
-        this.gameMovingObjects = [ this.cell ]
+        this.gameStaticObjects.push(this.background)
+    }
+
+    addCell() {
+        let newCell = new Cell(this)
+        this.gameMovingObjects.push(newCell)
+        new InputHandler(newCell)
     }
     
     update(deltaTime) {
--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -26,17 +26,9 @@ document.addEventListener('DOMContentLoaded', () => {
 
 
 window.onload = function() {
-    // Disable arrow key scrolling
-    // Source : https://stackoverflow.com/a/8916697/7974948
-    document.addEventListener("keydown", function (e) {
-        if ([37, 38, 39, 40].indexOf(e.keyCode) > -1) {
-            e.preventDefault();
-        }
-    }, false);
-
     const canvas = this.document.getElementById('game-canvas')
     let ctx = canvas.getContext('2d')
-    
+
     const padding = 10
     const GAME_HEIGHT = 500
     const CELL_HEIGHT = (GAME_HEIGHT / 4) - padding
@@ -44,13 +36,23 @@ window.onload = function() {
 
     let game = new Game(GAME_HEIGHT, CELL_HEIGHT, padding)
     game.start()
+    game.addCell()
+
+    // Disable arrow key scrolling
+    // Source : https://stackoverflow.com/a/8916697/7974948
+    document.addEventListener("keydown", function (e) {
+        if ([37, 38, 39, 40].indexOf(e.keyCode) > -1) {
+            game.addCell();
+            e.preventDefault();
+        }
+    }, false);
     
     let lastTime = 0;
     function gameLoop(timeStamp) {
         let deltaTime = timeStamp - lastTime
         lastTime = timeStamp
         ctx.clearRect(0, 0, GAME_HEIGHT, GAME_HEIGHT)
-        
+    
         game.update(deltaTime)
         game.draw(ctx)
 
